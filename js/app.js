c4930b570db9b717af8ba3f7ae469f9f20a21741
Hugo <hhugo.avila97@gmail.com>
2023-07-29 13:46:38 -0400

    agregando boton para eliminar y mostrar toda la informacion

--- js/app.js
+++ js/app.js
@@ -6,16 +6,16 @@ const listaCursos = document.querySelector('#lista-cursos');
 let articulosCarrito = [];
 
 cargarEventListeners();
-function cargarEventListeners(){
+function cargarEventListeners() {
     //Cuando agregas un curso presionando "Agregar al Carrito"
     listaCursos.addEventListener('click', agregarCurso);
 
 }
 
 //Funciones
-function agregarCurso(e){
+function agregarCurso(e) {
     e.preventDefault();
-    if(e.target.classList.contains('agregar-carrito')){
+    if (e.target.classList.contains('agregar-carrito')) {
         const cursoSeleccionado = e.target.parentElement.parentElement;
         leerDatosCurso(cursoSeleccionado);
     }
@@ -23,7 +23,7 @@ function agregarCurso(e){
 
 //Lee el contenido del HTML al que le dimos click y extrae la informacion del curso
 function leerDatosCurso(curso) {
-     console.log(curso);
+    console.log(curso);
 
     //Crear un objeto con el contenido del curso actual
     const infoCurso = {
@@ -35,6 +35,7 @@ function leerDatosCurso(curso) {
     }
     //Agrega elementos al arreglo de carrito
     articulosCarrito = [...articulosCarrito, infoCurso];
+
     console.log(articulosCarrito)
     carritoHTML();
 }
@@ -45,13 +46,20 @@ function carritoHTML() {
     limpiarHTML();
 
     //Recorre el carrito y genera el HTML
-    articulosCarrito.forEach( curso => {
-        
+    articulosCarrito.forEach(curso => {
+        const {imagen, titulo, precio, cantidad, id} = curso;
         const row = document.createElement('tr');
-       
+
         row.innerHTML = `
-            <td>${curso.imagen}</td>
-            <td>${curso.titulo}</td>
+            <td>
+                <img src=${imagen} width='100'>
+            </td>
+            <td>${titulo}</td>
+            <td>${precio}</td>
+            <td>${cantidad}</td>
+            <td>
+                <a href='#' class='borrar-curso' data-id=${id}>X</a>    
+            </td>
 
         `;
         //Agrega el HTML del carrito en el tbody
@@ -60,11 +68,11 @@ function carritoHTML() {
 }
 
 //Elimina los curso del tbody
-function limpiarHTML(){
+function limpiarHTML() {
     //Forma lenta
     //contenedorCarrito.innerHTML = '';
 
-    while(contenedorCarrito.firstChild){
+    while (contenedorCarrito.firstChild) {
         contenedorCarrito.removeChild(contenedorCarrito.firstChild);
     }
 }
